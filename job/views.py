8d07b1a190ca6f03f6089fad5b51c86ff4e31e2f
Raj-rajneesh <errajneesh007@gmail.com>
2023-05-22 12:53:08 +0530

    Add extra dots in view.py

--- job/views.py
+++ job/views.py
@@ -3,7 +3,7 @@ from .models import *
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 
-# Create your views here.
+# Create your views here......
 def index(request):
     return render(request,'index.html')
 
